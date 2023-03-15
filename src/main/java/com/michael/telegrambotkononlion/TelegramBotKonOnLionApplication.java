072e8aa614ab2c609bfa4fd217d14d790e32ac1f
leush <leushkinml@mail.ru>
2023-03-15 12:59:08 +0300

    Initial commit

---
+++ src/main/java/com/michael/telegrambotkononlion/TelegramBotKonOnLionApplication.java
@@ -0,0 +1,13 @@
+package com.michael.telegrambotkononlion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotKonOnLionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotKonOnLionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michael/telegrambotkononlion/TelegramBotKonOnLionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.telegrambotkononlion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotKonOnLionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
